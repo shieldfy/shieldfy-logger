25cdca87cc36ae62a01ccfa3ace3d58399fecea6
7ameed <hamid.mu7mad@gmail.com>
2019-12-18 13:16:04 +0200

    fixing the create logger issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /**
  * used to stream logs to elasticsearch server
  * @param {Object} config {
- *      
+ *      service
  *      host
  *      env 
  * } 
@@ -22,6 +22,32 @@ const shieldfyLogger = (config = {}) => {
 
     const winston = require('winston');
     const Elasticsearch = require('winston-elasticsearch');
+
+    // creating winston transport console instance
+    const winstonTransportConsole = new winston.transports.Console({ level: 'info', format: winston.format.simple() })
+
+    // logger 
+    winston.configure({
+        level: 'info',
+        format: winston.format.json(),
+        transports: [
+            winstonTransportConsole //log on console
+        ],
+        exitOnError: false // without it winston stops login after the first uncaught exception
+    });
+
+    /**
+     * in case of local or test environment or no env at all
+     */
+    if (env === 'local' || env === 'test' || !env) return winston
+    
+    // creating winston transport file instance
+    const winstonTransportFile = new winston.transports.File({ filename: "logfile.log", level: 'error' })
+    /**
+     * in case of non local or test environment
+     * add the File log
+    */
+    winston.add(winstonTransportFile);
 
     // elastic search config
     const esTransportOpts = {
@@ -46,38 +72,15 @@ const shieldfyLogger = (config = {}) => {
     // creating es instance
     const es = new Elasticsearch(esTransportOpts)
 
-    // creating winston transport file instance
-    const winstonTransportFile = new winston.transports.File({ filename: "logfile.log", level: 'error' })
-
-    // creating winston transport console instance
-    const winstonTransportConsole = new winston.transports.Console({ format: winston.format.simple() })
-
-    // logger 
-    const logger = winston.createLogger({
-        level: 'info',
-        format: winston.format.json(),
-        transports: [
-            winstonTransportFile, //save errors on file
-            winstonTransportConsole //log on console
-        ],
-        exitOnError: false // without it winston stops login after the first uncaught exception
-    });
-
     /**
-     * in case of local environment or no env at all
-     */
-    if (env === 'local' || !env) return logger
-
-    /**
-     * in case of non local environment 
+     * in case of non local or test environment 
      * add the elasticsearch log
      * and remove the console log
      */
+    winston.add(es);
+    winston.remove(winstonTransportConsole);
 
-    logger.remove(winstonTransportConsole);
-    logger.add(es);
-
-    return logger
+    return winston
 }
 
 module.exports = shieldfyLogger
